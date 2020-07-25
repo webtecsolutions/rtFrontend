5bf528ff9d5d39a04198b9f83e2ad6d8b1c4d388
Shivam Goswami <shiv.goswami@actewagl.com.au>
2020-07-25 17:42:59 +1000

    Finished home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -51,6 +51,11 @@
         </div>
 
         <div class="demo-section">
+            <div class="shape-layer">
+                <img src="../assets/Home/demo-shape1.svg" alt="Shape 1">
+                <img src="../assets/Home/demo-shape2.svg" alt="Shape 2">
+                <img src="../assets/Home/demo-shape3.svg" alt="Shape 3">
+            </div>
             <v-container>
                 <div class="iphone-container">
                     <img src="../assets/Home/iphone.png" alt="Get record time demo"/>
@@ -69,10 +74,38 @@
                 </div>
             </v-container>
         </div>
+
+        <v-container class="testimonial-section">
+            <p>Complete Docketing Solution</p>
+
+            <h2>Core Features of Record Time</h2>
+
+            <v-window v-model="testimonialWindow" vertical show-arrows :prev-icon="mdiArrowLeft" :next-icon="mdiArrowRight">
+                <v-window-item v-for="(chunkedTestimonial, index1) in computedTestimonials" :key="index1">
+                    <v-row no-gutters>
+                        <v-col :cols="12/(chunkedTestimonial.length)" v-for="(testimonial, index2) in chunkedTestimonial" :key="index2">
+                            <v-card class="testimonial-card">
+                                <v-card-text>
+                                    "{{testimonial.content}}"
+                                </v-card-text>
+
+                                <v-card-actions>
+                                    <v-avatar size="80">
+                                        <v-img :src="require('@/assets/Home/Testimonials/' + testimonial.image)"/>
+                                    </v-avatar>
+                                    <p>{{testimonial.jobTitle}}</p>
+                                    <p>{{testimonial.name}}</p>
+                                </v-card-actions>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-window-item>
+            </v-window>
+        </v-container>
     </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
     .features-section {
         background-color: $alice-grey;
         margin-top: 110px;
@@ -213,6 +246,42 @@
     .demo-section {
         height: 340px;
         background-color: $faux-oxford-blue;
+        position: relative;
+
+        .shape-layer {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            overflow: hidden;
+
+            img {
+                &:nth-of-type(1) {
+                    position: absolute;
+                    top: -20px;
+                    left: -110px;
+                }
+
+                &:nth-of-type(2) {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    transform: translateX(14%) translateY(-40%);
+                }
+
+                &:nth-of-type(3) {
+                    position: absolute;
+                    top: 0;
+                    right: 0;
+                    transform: translateX(40%) translateY(-6%);
+
+                    @media only screen and (max-width: 600px) {
+                        top: unset;
+                        bottom: 0;
+                        transform: translateX(10%) translateY(60%);
+                    }
+                }
+            }
+        }
 
         @media only screen and (max-width: 600px) {
             height: unset;
@@ -223,7 +292,7 @@
             padding-top: 66px;
             padding-bottom: 66px;
             display: flex;
-            justify-content: center;
+            justify-content: space-around;
             position: relative;
 
             @media only screen and (max-width: 600px) {
@@ -337,11 +406,123 @@
             }
         }
     }
+
+    .testimonial-section {
+        margin-top: 110px;
+        margin-bottom: 70px;
+        padding-top: 40px;
+        & > p {
+            opacity: 0.3;
+            letter-spacing: 4px;
+            text-align: center;
+            color: $black-pearl;
+            margin: 0;
+            padding-top: 0;
+            text-transform: uppercase;
+            padding-bottom: 10px;
+        }
+        & > h2 {
+            font-size: 32px;
+            font-weight: bold;
+            text-align: center;
+            color: #021222;
+            padding-bottom: 40px;
+        }
+
+        div.v-window.v-item-group {
+            .v-window__container {
+                position: relative;
+
+                [class*="next"], [class*="prev"] {
+                    background: transparent;
+                    position: relative;
+
+                    > button.v-btn {
+                        position: absolute;
+                        bottom: -70px;
+                        .v-icon {
+                            color: $black-pearl;
+                        }
+                    }
+                }
+
+                [class*="next"] {
+                    > button.v-btn {
+                        right: 0;
+                    }
+                }
+
+                [class*="prev"] {
+                    > button.v-btn {
+                        right: 50px;
+                    }
+                }
+
+                .v-window-item {
+                    .row {
+                        .col {
+                            padding: 0 10px;
+
+                            .testimonial-card {
+                                padding: 0;
+                                border-radius: 5px;
+                                box-shadow: 0 2px 12px 0 rgba(2, 18, 34, 0.08);
+
+                                div.v-card__text {
+                                    padding: 21px 30px 63px 30px;
+                                    font-size: 14px;
+                                    font-style: italic;
+                                    line-height: 1.71;
+                                    color: $black-pearl;
+                                }
+
+                                div.v-card__actions {
+                                    padding: 48px 0 36px 0;
+                                    background-color: $faux-dark-turquoise;
+                                    border-radius: 5px;
+                                    position: relative;
+                                    display: flex;
+                                    flex-direction: column;
+
+                                    > div.v-avatar {
+                                        position: absolute;
+                                        top: 0;
+                                        left: 50%;
+                                        transform: translateX(-50%) translateY(-50%);
+                                    }
+
+                                    p {
+                                        padding: 0;
+                                        margin: 0;
+                                        text-align: center;
+
+                                        &:nth-of-type(1) {
+                                            font-size: 18px;
+                                            font-weight: 600;
+                                            color: #ffffff;
+                                        }
+
+                                        &:nth-of-type(2) {
+                                            opacity: 0.8;
+                                            font-size: 14px;
+                                            color: #ffffff;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 </style>
 
 <script>
     // @ is an alias to /src
     import LandingBanner from '@/components/LandingBanner.vue';
+    import { mdiArrowLeft } from '@mdi/js';
+    import { mdiArrowRight } from '@mdi/js';
 
     export default {
         metaInfo: {
@@ -357,6 +538,89 @@
         },
         data() {
             return {
+                testimonialWindow: 0,
+                mdiArrowLeft: mdiArrowLeft,
+                mdiArrowRight: mdiArrowRight,
+                testimonials: [
+                    {
+                        content: "Now that safety on work sites (particularly in construction), is such a big issue for employers to" +
+                            "be aware of, my employees can also customize the dockets to become a Job Safety Assessment (JSA)," +
+                            " that can be filled out before working on site. This covers both employers and employees in case of" +
+                            " a safety breach, and again, it is all accessible on the back end. The JSA can be sent to both the supervisor and the client.",
+                        image: "1.jpg",
+                        jobTitle: "CEO at Fortify Geotech",
+                        name: "Dean Robinson"
+                    },
+                    {
+                        content: "Now that safety on work sites (particularly in construction), is such a big issue for employers to" +
+                            "be aware of, my employees can also customize the dockets to become a Job Safety Assessment (JSA)," +
+                            " that can be filled out before working on site. This covers both employers and employees in case of" +
+                            " a safety breach, and again, it is all accessible on the back end. The JSA can be sent to both the supervisor and the client.",
+                        image: "1.jpg",
+                        jobTitle: "CEO at Fortify Geotech",
+                        name: "Dean Robinson"
+                    },
+                    {
+                        content: "Now that safety on work sites (particularly in construction), is such a big issue for employers to" +
+                            "be aware of, my employees can also customize the dockets to become a Job Safety Assessment (JSA)," +
+                            " that can be filled out before working on site. This covers both employers and employees in case of" +
+                            " a safety breach, and again, it is all accessible on the back end. The JSA can be sent to both the supervisor and the client.",
+                        image: "1.jpg",
+                        jobTitle: "CEO at Fortify Geotech",
+                        name: "Dean Robinson"
+                    },
+                    {
+                        content: "Now that safety on work sites (particularly in construction), is such a big issue for employers to" +
+                            "be aware of, my employees can also customize the dockets to become a Job Safety Assessment (JSA)," +
+                            " that can be filled out before working on site. This covers both employers and employees in case of" +
+                            " a safety breach, and again, it is all accessible on the back end. The JSA can be sent to both the supervisor and the client.",
+                        image: "1.jpg",
+                        jobTitle: "CEO at Fortify Geotech",
+                        name: "Dean Robinson"
+                    },
+                    {
+                        content: "Now that safety on work sites (particularly in construction), is such a big issue for employers to" +
+                            "be aware of, my employees can also customize the dockets to become a Job Safety Assessment (JSA)," +
+                            " that can be filled out before working on site. This covers both employers and employees in case of" +
+                            " a safety breach, and again, it is all accessible on the back end. The JSA can be sent to both the supervisor and the client.",
+                        image: "1.jpg",
+                        jobTitle: "CEO at Fortify Geotech",
+                        name: "Dean Robinson"
+                    },
+                    {
+                        content: "Now that safety on work sites (particularly in construction), is such a big issue for employers to" +
+                            "be aware of, my employees can also customize the dockets to become a Job Safety Assessment (JSA)," +
+                            " that can be filled out before working on site. This covers both employers and employees in case of" +
+                            " a safety breach, and again, it is all accessible on the back end. The JSA can be sent to both the supervisor and the client.",
+                        image: "1.jpg",
+                        jobTitle: "CEO at Fortify Geotech",
+                        name: "Dean Robinson"
+                    }
+                ]
+            }
+        },
+        computed: {
+            computedTestimonials() {
+                let result = [];
+                if (this.$vuetify.breakpoint.lgAndUp) {
+                    result = this.chunkArray(this.testimonials, 3);
+                } else if (this.$vuetify.breakpoint.mdOnly) {
+                    result = this.chunkArray(this.testimonials, 3);
+                } else if (this.$vuetify.breakpoint.smOnly) {
+                    result = this.chunkArray(this.testimonials, 2);
+                } else if (this.$vuetify.breakpoint.xsOnly) {
+                    result = this.chunkArray(this.testimonials, 1);
+                }
+                return result;
+            }
+        },
+        methods: {
+            chunkArray(myArray, chunk_size) {
+                let results = [];
+                while (myArray.length) {
+                    results.push(myArray.splice(0, chunk_size));
+                }
+                return results;
             }
         }
     }
